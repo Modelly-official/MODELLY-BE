ded1e5cd4e089ffb2e58237485c18f558655e394
dldusgh318 <pololydotoly@gmail.com>
2025-10-14 23:36:21 +0900

    chore: 초기 환경 세팅 (#1)

---
+++ src/test/java/modelly/modelly_be/ModellyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package modelly.modelly_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModellyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
