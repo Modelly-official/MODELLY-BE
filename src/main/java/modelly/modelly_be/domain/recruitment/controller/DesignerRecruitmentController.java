d64866f11e50f1378f5c877254cbc781303dfbfa
dldusgh318 <pololydotoly@gmail.com>
2025-11-28 18:10:50 +0900

    feat: 공고 삭제 api 구현 (#18)

--- src/main/java/modelly/modelly_be/domain/recruitment/controller/DesignerRecruitmentController.java
+++ src/main/java/modelly/modelly_be/domain/recruitment/controller/DesignerRecruitmentController.java
@@ -1,19 +1,15 @@
 package modelly.modelly_be.domain.recruitment.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import modelly.modelly_be.domain.recruitment.dto.RecruitmentRequestDto;
-import modelly.modelly_be.domain.recruitment.dto.RecruitmentResponseDto;
+import modelly.modelly_be.domain.recruitment.dto.request.RecruitmentRequestDto;
+import modelly.modelly_be.domain.recruitment.dto.response.RecruitmentResponseDto;
 import modelly.modelly_be.domain.recruitment.entity.Recruitment;
 import modelly.modelly_be.domain.recruitment.service.DesignerRecruitmentService;
 import modelly.modelly_be.global.apiPayload.ApiResponse;
 import modelly.modelly_be.global.security.AuthDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,4 +27,23 @@ public class DesignerRecruitmentController implements DesignerRecruitmentSwagger
 
         return ApiResponse.onSuccess(responseDto);
     }
+
+    //공고 수정
+    @PutMapping("/recruitments")
+    public ApiResponse<String> updateRecruitment(@AuthenticationPrincipal AuthDetails authDetails, @RequestBody @Valid RecruitmentRequestDto recruitmentRequestDto){
+        //Recruitment recruitment = designerRecruitmentService.updateRecruitment(authDetails.user(), recruitmentRequestDto);
+
+        //RecruitmentResponseDto responseDto = RecruitmentResponseDto.from(recruitment);
+        return ApiResponse.onSuccess("수정이 완료되었습니다.");
+    }
+
+    //공고 삭제
+    @DeleteMapping("/recruitments/{recruitmentId}")
+    public ApiResponse<String> deleteRecruitment(@AuthenticationPrincipal AuthDetails authDetails, @PathVariable Long recruitmentId){
+        designerRecruitmentService.deleteRecruitment(authDetails.user(),recruitmentId);
+        return ApiResponse.onSuccess("공고가 삭제되었습니다.");
+    }
+
+
+    //내 공고 리스트 조회하기
 }
--- src/main/java/modelly/modelly_be/domain/recruitment/controller/DesignerRecruitmentSwagger.java
+++ src/main/java/modelly/modelly_be/domain/recruitment/controller/DesignerRecruitmentSwagger.java
@@ -3,16 +3,25 @@ package modelly.modelly_be.domain.recruitment.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import modelly.modelly_be.domain.recruitment.dto.RecruitmentRequestDto;
-import modelly.modelly_be.domain.recruitment.dto.RecruitmentResponseDto;
+import modelly.modelly_be.domain.recruitment.dto.request.RecruitmentRequestDto;
+import modelly.modelly_be.domain.recruitment.dto.response.RecruitmentResponseDto;
 import modelly.modelly_be.global.apiPayload.ApiResponse;
 import modelly.modelly_be.global.security.AuthDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "디자이너 공고관련 API", description = "공고 CRUD, 내 공고 리스트 조회")
+@Tag(name = "디자이너 공고관련 API", description = "공고 CUD, 내 공고 리스트 조회")
 public interface DesignerRecruitmentSwagger {
 
     @Operation(summary = "공고 생성하기", description = "디자이너가 공고 생성 시 사용하는 API입니다.")
     ApiResponse<RecruitmentResponseDto> createRecruitment(@AuthenticationPrincipal AuthDetails authDetails, @RequestBody @Valid RecruitmentRequestDto recruitmentRequestDto);
-}
+
+    @Operation(summary = "공고 수정하기", description = "디자이너가 공고 수정 시 사용하는 API입니다. (아직 구현 X)")
+    ApiResponse<String> updateRecruitment(@AuthenticationPrincipal AuthDetails authDetails, @RequestBody @Valid RecruitmentRequestDto recruitmentRequestDto);
+
+    @Operation(summary = "공고 삭제하기", description = "디자이너가 공고 삭제 시 사용하는 API입니다.")
+    ApiResponse<String> deleteRecruitment(@AuthenticationPrincipal AuthDetails authDetails, @PathVariable Long recruitmentId);
+
+
+    }
