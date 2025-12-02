3bfd98b6b2d5323055554c7c8053cce43e40ca21
Junyoung Lee <junily@yonsei.ac.kr>
2025-12-03 00:40:41 +0900

    feat: 채팅 이미지 S3 업로드 및 이미지 전송 처리 (#21)

--- src/main/java/modelly/modelly_be/domain/chat/entity/enums/MessageType.java
+++ src/main/java/modelly/modelly_be/domain/chat/entity/enums/MessageType.java
@@ -2,8 +2,12 @@ package modelly.modelly_be.domain.chat.entity.enums;
 
 public enum MessageType {
     TEXT("텍스트"),
+<<<<<<< HEAD
     IMAGE("이미지"),
     READ("읽음 여부")
+=======
+    IMAGE("이미지")
+>>>>>>> 7b5ac96 (feat: 채팅 이미지 S3 업로드 및 이미지 전송 처리 (#21))
     ;
 
     private final String description;
