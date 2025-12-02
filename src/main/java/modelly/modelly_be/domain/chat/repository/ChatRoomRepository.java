1e90ee8daf87e04cdf669e6a182b9371c2af59fb
Junyoung Lee <junily@yonsei.ac.kr>
2025-12-02 14:22:24 +0900

    refactor: Param annotation import 오류 수정 (#15)

--- src/main/java/modelly/modelly_be/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/modelly/modelly_be/domain/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,5 @@
 package modelly.modelly_be.domain.chat.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import modelly.modelly_be.domain.chat.entity.ChatRoom;
 import modelly.modelly_be.domain.chat.entity.Chatting;
 import modelly.modelly_be.domain.user.entity.Designer;
@@ -9,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
