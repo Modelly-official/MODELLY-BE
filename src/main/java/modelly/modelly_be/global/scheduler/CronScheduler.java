1d56f1c0a62d139705f8e298135cd004b1af1de6
dldusgh318 <pololydotoly@gmail.com>
2025-12-05 08:53:41 +0900

    feat: 공고 마감 스케줄링 구현 (27)

---
+++ src/main/java/modelly/modelly_be/global/scheduler/CronScheduler.java
@@ -0,0 +1,30 @@
+package modelly.modelly_be.global.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import modelly.modelly_be.domain.recruitment.service.RecruitmentService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CronScheduler {
+
+    private final RecruitmentService recruitmentService;
+
+    @Scheduled(fixedRate = 5000)
+    //@Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void updatePostStatus(){
+        try{
+            LocalDate today = LocalDate.now();
+            int updated = recruitmentService.updateStatusToClosed(today);
+            log.info("[scheduler] 마감기한이 지난 공고글 상태 업데이트 -> 총 {}건", updated);
+        } catch (Exception e){
+            log.error("[scheduler] 🚨ERROR | message: {}", e.getMessage());
+        }
+
+    }
+}
