3f1a657076788fb03eea8bc53e4a5b5a2657af86
dldusgh318 <pololydotoly@gmail.com>
2025-12-08 17:14:53 +0900

    docs: swagger 정리 (#31)

--- src/main/java/modelly/modelly_be/domain/like/controller/swagger/LikeSwagger.java
+++ src/main/java/modelly/modelly_be/domain/like/controller/swagger/LikeSwagger.java
@@ -18,7 +18,12 @@ public interface LikeSwagger {
     @Operation(summary = "공고 찜하기 / 찜 취소하기", description = "모델이 공고를 찜하거나 찜을 취소할 때 사용하는 API입니다.")
     ApiResponse<String> recruitmentLikeOrLikeCancel(@AuthenticationPrincipal AuthDetails authDetails, @PathVariable Long recruitmentId);
 
-    @Operation(summary = "찜한 공고 리스트 조회하기", description = "모델이 찜한 공고리스트를 조회할 때 사용하는 API입니다.")
+    @Operation(summary = "찜한 공고 리스트 조회하기", description = """
+            모델이 찜한 공고리스트를 조회할 때 사용하는 API입니다. \n
+            `category` : HAIR, NAIL, TATTOO, EYELASH, ETC 중 택1 \n
+            `cursorId` : 다음 페이지를 보여줄 경우 responseDto에서의 nextCursor 값을 넣어주시면 됩니다. \n
+            `size` : 한 페이지에서 보여질 찜한 공고의 개수 \n
+            """)
     ApiResponse<ScrollResponse<LikeRecruitmentListResponseDto>> getLikeRecruitmentList(
             @AuthenticationPrincipal AuthDetails authDetails,
             @RequestParam(required = false) Category category,
@@ -28,7 +33,12 @@ public interface LikeSwagger {
     @Operation(summary = "디자이너 찜하기 / 찜 취소하기", description = "모델이 디자이너를 찜하거나 찜을 취소할 때 사용하는 API입니다.")
     ApiResponse<String> designerLikeOrLikeCancel(@AuthenticationPrincipal AuthDetails authDetails, @PathVariable Long designerId);
 
-    @Operation(summary = "찜한 디자이너 리스트 조회하기", description = "모델이 찜한 디자이너 리스트를 조회할 때 사용하는 API입니다.")
+    @Operation(summary = "찜한 디자이너 리스트 조회하기", description = """
+            모델이 찜한 디자이너 리스트를 조회할 때 사용하는 API입니다. \n
+            `category` : HAIR, NAIL, TATTOO, EYELASH, ETC 중 택1 \n
+            `cursorId` : 다음 페이지를 보여줄 경우 responseDto에서의 nextCursor 값을 넣어주시면 됩니다. \n
+            `size` : 한 페이지에서 보여질 찜한 디자이너의 수 \n
+            """)
     ApiResponse<ScrollResponse<LikeDesignerListResponseDto>> getLikeDesignerList(
             @AuthenticationPrincipal AuthDetails authDetails,
             @RequestParam(required = false) Category category,
