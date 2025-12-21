229d4960ee498d3b7b10af535bf13cd400af9b12
Junyoung Lee <junily@yonsei.ac.kr>
2025-12-21 21:02:41 +0900

    fix: 채팅 상대방 디자이너 정보 활동명으로 변경 (#47)

--- src/main/java/modelly/modelly_be/domain/chat/service/ChatRoomService.java
+++ src/main/java/modelly/modelly_be/domain/chat/service/ChatRoomService.java
@@ -194,7 +194,7 @@ public class ChatRoomService {
         if (iAmModel) {
             User designerUser = designer.getUser();
             opponentUserId = designerUser.getId();
-            opponentName = designerUser.getName();          // 활동명
+            opponentName = designer.getNickname();          // 활동명
             opponentProfileImageUrl = designerUser.getImageUrl();
             opponentRole = UserRole.DESIGNER;
         }
