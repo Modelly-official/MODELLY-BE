c3da1431fa5aee94f4bba18a485daf2a1144a023
Junyoung Lee <junily@yonsei.ac.kr>
2025-12-23 23:31:16 +0900

    feat: ReservationImage  FetchType을 LAZY로 변경(#55)

--- src/main/java/modelly/modelly_be/domain/reservation/entity/ReservationImage.java
+++ src/main/java/modelly/modelly_be/domain/reservation/entity/ReservationImage.java
@@ -14,7 +14,7 @@ public class ReservationImage extends BaseEntity {
     @Column(name = "reservation_image_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reservation_id", nullable = false)
     private Reservation reservation;
 
