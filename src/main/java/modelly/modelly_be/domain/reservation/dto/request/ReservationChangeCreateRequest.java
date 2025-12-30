5c89044679ff6e9285020ec76e5b2d043dd11d30
Junyoung Lee <junily@yonsei.ac.kr>
2025-12-30 16:25:36 +0900

    refactor: Reservation Request Dto message 추가 (#59)

--- src/main/java/modelly/modelly_be/domain/reservation/dto/request/ReservationChangeCreateRequest.java
+++ src/main/java/modelly/modelly_be/domain/reservation/dto/request/ReservationChangeCreateRequest.java
@@ -6,7 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ReservationChangeCreateRequest(
-        @NotNull LocalDate proposedDate,     // 2025-12-13
-        @NotBlank String proposedStartTime,  // "13:30"
-        @NotNull String reason
+        @NotNull(message = "변경 희망 날짜는 필수입니다.")
+        LocalDate proposedDate,// 2025-12-13
+        @NotBlank(message = "변경 희망 시작 시간은 필수입니다.")
+        String proposedStartTime,  // "13:30"
+        @NotBlank(message = "변경 사유는 필수입니다.")
+        String reason
 ) {}
