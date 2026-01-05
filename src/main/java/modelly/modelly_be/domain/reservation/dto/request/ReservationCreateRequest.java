e766296b5a6d58ba2dc64d0866919580cf20a701
Junyoung Lee <junily@yonsei.ac.kr>
2026-01-05 18:26:06 +0900

    refactor: 예약 시 imageUrl 필수 항목에서 제외 (#73)

--- src/main/java/modelly/modelly_be/domain/reservation/dto/request/ReservationCreateRequest.java
+++ src/main/java/modelly/modelly_be/domain/reservation/dto/request/ReservationCreateRequest.java
@@ -35,6 +35,5 @@ public record ReservationCreateRequest(
         @NotBlank(message = "샵 이름은 필수입니다.")
         String shop,
 
-        @NotBlank(message = "이미지 URL은 필수입니다.")
         String imageUrls
 ) {}
