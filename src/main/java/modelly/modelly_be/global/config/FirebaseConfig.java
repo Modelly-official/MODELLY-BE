1701ac03127c275909296c84599c636889c1846a
dldusgh318 <pololydotoly@gmail.com>
2026-01-06 18:06:56 +0900

    chore: firebase 연동 (#75)

---
+++ src/main/java/modelly/modelly_be/global/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package modelly.modelly_be.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+        InputStream serviceAccount = getClass().getResourceAsStream("/moandi_serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) { // 기존에 FirebaseApp이 초기화되지 않은 경우만 실행
+            FirebaseApp.initializeApp(options);
+        }
+
+    }
+}
