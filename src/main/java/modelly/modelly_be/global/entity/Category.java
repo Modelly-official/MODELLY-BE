2c09186f639b0ef3603235ded5155e8c048e00a0
dldusgh318 <pololydotoly@gmail.com>
2026-01-10 16:58:28 +0900

    fix: category enum->string으로 응답값 변환

--- src/main/java/modelly/modelly_be/global/entity/Category.java
+++ src/main/java/modelly/modelly_be/global/entity/Category.java
@@ -1,5 +1,7 @@
 package modelly.modelly_be.global.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Category {
     HAIR("헤어"),
     NAIL("네일"),
@@ -11,6 +13,7 @@ public enum Category {
 
     Category(String description) {this.description = description;}
 
+    @JsonValue
     public String getDescription() {
         return description;
     }
